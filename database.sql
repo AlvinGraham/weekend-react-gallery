a9b305feb6f03cf22a190d371a863c02ff03253e
Alvin Graham <grah0071@gmail.com>
2024-01-27 22:15:47 -0600

    Create and test server GET route

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS "gallery";
+
 CREATE TABLE "gallery" (
   "id" SERIAL PRIMARY KEY,
   "url" VARCHAR,
@@ -9,5 +11,8 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
-('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
-  
+('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
+('images/DwarfRanger.jpg', 'Ranger', 'Image of a Dwarven Ranger for use in Pathfinder'),
+('images/GrahamBoys.jpg', 'Graham Boys', '3 generations of Grahams at a Vikings game in LA'),
+('images/titanite.jpg', 'Titanite', 'My Prime Academy cohort''s namesake')  ;
+ 
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const pool = require("../modules/pool");
 
 // PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
+router.put("/like/:id", (req, res) => {
   // code here
 });
 
 // GET /gallery
-router.get('/', (req, res) => {
-  // code here
+router.get("/", (req, res) => {
+  // Create queryText
+  const queryText = `SELECT * FROM "gallery" ORDER BY id;`;
+
+  // conduct query
+  pool
+    .query(queryText)
+    .then((result) => {
+      console.log("Successful GET DB Query");
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR in GET DB Query:", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
