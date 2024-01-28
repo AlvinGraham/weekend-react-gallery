644e09a5a63b65ff1eb92d15339e0b7a0644d2c3
Alvin Graham <grah0071@gmail.com>
2024-01-28 06:33:37 -0600

    Add initial styling of galleryItem component

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import "./GalleryItem.css";
+
 export default function GalleryItem({ galleryData, galleryRefreshCallback }) {
+  // initialize state variables
+  const [showImage, setShowImage] = useState(true);
+
   return (
-    <div data-testid="galleryItem">
+    <div
+      data-testid="galleryItem"
+      className="gallery-card">
+      <p>{galleryData.title}</p>
       <img
         src={galleryData.url}
         width="150px"
         height="150px"
       />
+
+      <button>Click to Like!</button>
+      <p>{galleryData.likes} people like this</p>
     </div>
   );
 }
