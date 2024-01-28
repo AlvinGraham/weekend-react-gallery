42561b3533bb1c8709496a1cd1143bb843e38a2d
Alvin Graham <grah0071@gmail.com>
2024-01-27 22:30:50 -0600

    Create and test server PUT route

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,7 +4,23 @@ const pool = require("../modules/pool");
 
 // PUT /gallery/like/:id
 router.put("/like/:id", (req, res) => {
-  // code here
+  // Create queryText and queryArgs
+  const queryText = `UPDATE "gallery" SET likes = likes + 1 WHERE id = $1;`;
+  const queryArgs = [req.params.id];
+
+  console.log(`DB Query: ${queryText}\nArgs: ${queryArgs}`);
+
+  // conduct query
+  pool
+    .query(queryText, queryArgs)
+    .then((result) => {
+      console.log("Successful PUT DB Query");
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error("ERROR in PUT DB Query:", err);
+      res.sendStatus(500);
+    });
 });
 
 // GET /gallery
@@ -20,7 +36,7 @@ router.get("/", (req, res) => {
       res.send(result.rows);
     })
     .catch((err) => {
-      console.log("ERROR in GET DB Query:", err);
+      console.error("ERROR in GET DB Query:", err);
       res.sendStatus(500);
     });
 });
